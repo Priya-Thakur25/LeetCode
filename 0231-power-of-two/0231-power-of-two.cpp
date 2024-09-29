457d7c26b45294dc12a1daf12480f944cfe7bfe9
Priya Thakur <165200711+Priya-Thakur25@users.noreply.github.com>
2024-09-29 07:01:20 +0530

    Time: 0 ms (100%), Space: 7.5 MB (26.94%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
-bool checkPower(int n){
-    if(n == 1 || n == 2) return true;
-    if(n%2 != 0 || n == 0) return false;
+// bool checkPower(int n){
+//     if(n == 1 || n == 2) return true;
+//     if(n%2 != 0 || n == 0) return false;
 
-    if(n%2 == 0){
-        n = n/2;
-    }
-    return checkPower(n);
-}
+//     if(n%2 == 0){
+//         n = n/2;
+//     }
+//     return checkPower(n);
+// }
     bool isPowerOfTwo(int n) {
-        if(checkPower(n)) return true;
-        else return false;
-    }
+    //     if(checkPower(n)) return true;
+    //     else return false;
+
+    if(n <= 0) return false;
+    if((n & (n-1)) == 0) return true;
+    else return false;
+     }
 };
