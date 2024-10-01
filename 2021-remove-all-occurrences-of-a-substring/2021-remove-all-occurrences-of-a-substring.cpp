4a46204e796bcacb3ff60596a97853909a88a040
Priya Thakur <165200711+Priya-Thakur25@users.noreply.github.com>
2024-10-01 11:25:38 +0530

    Time: 0 ms (100%), Space: 8.2 MB (23.96%) - LeetHub

--- 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.cpp
+++ 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.cpp
@@ -2,9 +2,10 @@ class Solution {
 public:
     string removeOccurrences(string s, string part) {
         int n = part.length();
-        while(s.find(part) != string::npos){
-            int i = s.find(part);
+        int i = s.find(part);
+        while(i != string::npos){
             s.erase(i,n);
+            i = s.find(part);
         }
         return  s;
     }
