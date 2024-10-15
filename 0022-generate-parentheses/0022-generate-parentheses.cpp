9f4d647450474497e86c2cd5bb1ba2ebccbd7d08
Priya Thakur <165200711+Priya-Thakur25@users.noreply.github.com>
2024-10-15 14:37:02 +0530

    Time: 6 ms (25.38%), Space: 12.3 MB (80.09%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,33 +1,56 @@
 class Solution {
 public:
 vector<string>final;
-bool isValid(string s){
-    int count = 0;
-    for(auto ch: s){
-        if(ch == '(') count ++;
-        else if(ch == ')') count--;
-        if(count < 0) return false;
-    }
-    return count == 0;
-}
-void solve(string &s, int n){
+//brute force
+// bool isValid(string s){
+//     int count = 0;
+//     for(auto ch: s){
+//         if(ch == '(') count ++;
+//         else if(ch == ')') count--;
+//         if(count < 0) return false;
+//     }
+//     return count == 0;
+// }
+// void solve(string &s, int n){
+//     if(s.length() == n*2){
+//         if(isValid(s)){
+//             final.push_back(s);
+//         }
+//         return;
+//     }
+
+//     s.push_back('(');
+//     solve(s, n--);
+//     s.pop_back();
+//     s.push_back(')');
+//     solve(s, n--);
+//     s.pop_back();
+// }
+
+//optimize
+void solve(string &s, int n, int open, int close){
     if(s.length() == n*2){
-        if(isValid(s)){
-            final.push_back(s);
-        }
+         final.push_back(s);
         return;
     }
 
-    s.push_back('(');
-    solve(s, n);
-    s.pop_back();
-    s.push_back(')');
-    solve(s, n);
-    s.pop_back();
+    if(open < n){
+        s.push_back('(');
+        solve(s, n, open+1, close);
+        s.pop_back();
+    }
+
+    if(close < open){
+        s.push_back(')');
+        solve(s, n, open, close+1);
+        s.pop_back();
+    }
 }
     vector<string> generateParenthesis(int n) {
         string s = "";
-        solve(s, n);
+        int open = 0;
+        int close = 0;
+        solve(s, n, open, close);
         return final;
     }
 };
