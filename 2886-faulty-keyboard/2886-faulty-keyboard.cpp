4f552fbbf55d7f90a7463fe3737320042bcda581
Priya Thakur <165200711+Priya-Thakur25@users.noreply.github.com>
2024-10-26 23:41:09 +0530

    Time: 3 ms (82.95%), Space: 9.7 MB (73.94%) - LeetHub

---
+++ 2886-faulty-keyboard/2886-faulty-keyboard.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string finalString(string s) {
+        int len = s.length();
+        int i = 0;
+        string ans;
+        while(i < len){
+            char ch  = s[i];
+            if(ch == 'i' && i != 0){
+                reverse(ans.begin(), ans.end());
+            }
+            else if(ch != 'i'){
+                ans.push_back(ch);
+            }
+            i++;
+        }
+        return ans;
+    }
+};
