e2c052f4bc6c6134ebc86f3f8f6bc19d7e9badd0
Priya Thakur <165200711+Priya-Thakur25@users.noreply.github.com>
2024-10-28 16:07:49 +0530

    Time: 0 ms (100%), Space: 20.6 MB (75.23%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -3,9 +3,7 @@ public:
     int missingNumber(vector<int>& nums) {
           int n = nums.size();
           int sum = 0, ans = 0;
-          for(int i=0; i<=n; i++){
-            sum = sum+i;
-          }
+          sum = (n*(n+1)/2);
           for(int i=0; i<n; i++){
             ans = ans+nums[i];
           }
