69948fadd93d1eee7694bdfe4da1ffdbd00c210a
Priya Thakur <165200711+Priya-Thakur25@users.noreply.github.com>
2024-10-28 18:11:54 +0530

    Time: 0 ms (100%), Space: 27.6 MB (50.17%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -3,18 +3,21 @@ public:
     void rotate(vector<int>& nums, int k) {
         int n = nums.size();
         k = k%n;
-        vector<int>ans;
-        for(int i=n-k; i<n; i++){
-            ans.push_back(nums[i]);
-        }
-        reverse(ans.begin(), ans.end());
-        for(int i=0; i<k; i++){
-            nums.pop_back();
-        }
-        reverse(nums.begin(), nums.end());
-        for(int i=0; i<k; i++){
-            nums.push_back(ans[i]);
-        }
-        reverse(nums.begin(), nums.end());
+    //     vector<int>ans;
+    //     for(int i=n-k; i<n; i++){
+    //         ans.push_back(nums[i]);
+    //     }
+    //     reverse(ans.begin(), ans.end());
+    //     for(int i=0; i<k; i++){
+    //         nums.pop_back();
+    //     }
+    //     reverse(nums.begin(), nums.end());
+    //     for(int i=0; i<k; i++){
+    //         nums.push_back(ans[i]);
+    //     }
+    //     reverse(nums.begin(), nums.end());
+    reverse(nums.begin()+(n-k), nums.end());
+    reverse(nums.begin(), nums.begin()+(n-k));
+    reverse(nums.begin(), nums.end());
     }
 };
