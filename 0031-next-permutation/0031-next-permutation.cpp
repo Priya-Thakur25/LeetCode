8602dc0fac761ec3c73b7cc2728997237fb8f66e
Priya Thakur <165200711+Priya-Thakur25@users.noreply.github.com>
2024-10-31 13:20:01 +0530

    Time: 0 ms (100%), Space: 14.9 MB (5.59%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,6 +1,27 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin(), nums.end());
+        // next_permutation(nums.begin(), nums.end());
+        //Good Approach
+        int index = -1, nextIndex = 0, n = nums.size();
+        for(int i=n-2; i>=0; i--){
+            if(nums[i] < nums[i+1]){
+                index = i;
+                break;
+            }
+        }
+        if(index != -1){
+            for(int i=n-1; i>index; i--){
+                if(nums[i] > nums[index]){
+                    nextIndex = i;
+                    break;
+                }
+            }
+            swap(nums[nextIndex], nums[index]);
+            reverse(nums.begin() + index + 1, nums.end());
+        }
+        else{
+            reverse(nums.begin(), nums.end());
+        }
     }
 };
