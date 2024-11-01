4e5394e64ccc8e301908476a49919508c4cc77a0
Priya Thakur <165200711+Priya-Thakur25@users.noreply.github.com>
2024-11-01 16:49:32 +0530

    Time: 0 ms (100%), Space: 27.3 MB (16.93%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -1,17 +1,10 @@
 class Solution {
 public:
-void call(vector<char>&s, int len, int i){
-    if(i >= len){
-        return;
-    }
-
-    swap(s[i],s[len]);
-    call(s,len-1,i+1);
-    return;
-}
     void reverseString(vector<char>& s) {
-        int len = s.size();
-        int i = 0;
-        call(s,len-1,i);
+        int n =  s.size();
+        int i=0, j=n-1;
+        for(i=0,j=n-1; i<j; i++,j--){
+            swap(s[i], s[j]);
+        }
     }
 };
