dd9768050d9cf16b83c2fb6c70b10653b8417061
Priya Thakur <165200711+Priya-Thakur25@users.noreply.github.com>
2024-11-02 13:19:50 +0530

    Time: 4 ms (5.45%), Space: 8.2 MB (83.91%) - LeetHub

---
+++ 2580-circular-sentence/2580-circular-sentence.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool isCircularSentence(string s) {
+        vector<char>str;
+        int n = s.length();
+        int i = 0;
+        while(i < n){
+            if(i == 0) str.push_back(s[i]);
+            if(i+1 < n && ((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z')) && s[i+1] == ' '){
+                str.push_back(s[i]);
+            }
+            if(i-1 >=0 && ((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z')) && s[i-1] == ' '){
+                str.push_back(s[i]);
+            }
+            
+            i++;
+        }
+        str.insert(str.begin(), s[n-1]);
+        int size = str.size();
+        i = 0;
+        while(i < size){
+            cout << str[i] << endl;
+            if(str[i] != str[i+1]){
+                return false;
+            }
+            i += 2;
+        }
+        return true;
+    }
+};
