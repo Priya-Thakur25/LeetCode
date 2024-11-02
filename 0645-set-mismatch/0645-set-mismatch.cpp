0888e1d6def5657765d2303d4f902b313f349bed
Priya Thakur <165200711+Priya-Thakur25@users.noreply.github.com>
2024-11-02 23:29:04 +0530

    Time: 0 ms (100%), Space: 25 MB (53.85%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.cpp
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -1,27 +1,42 @@
 class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        set<int>setting(nums.begin(), nums.end());
-        int dupli = 0, i = 0 , n = nums.size();
-        vector<int>ans; 
-        for(i=0; i<n-1; i++){
-           if(nums[i] == nums[i+1]){
-            ans.push_back(nums[i]);
-            break;
-           }
+        // sort(nums.begin(), nums.end());
+        // set<int>setting(nums.begin(), nums.end());
+        // int dupli = 0, i = 0 , n = nums.size();
+        // vector<int>ans; 
+        // for(i=0; i<n-1; i++){
+        //    if(nums[i] == nums[i+1]){
+        //     ans.push_back(nums[i]);
+        //     break;
+        //    }
+        // }
+        // i=0;
+        // for(auto sett: setting){
+        //     if(i+1 != sett){
+        //         ans.push_back(i+1);
+        //         break;
+        //     }
+        //     i++;
+        // }
+        // if(ans.size() != 2){
+        //     ans.push_back(nums.size());
+        // }
+        // return ans;
+        int n = nums.size();
+        vector<int>count(n+1, 0);
+        int missing = -1, dupli = -1;
+        for(auto num:nums){
+            count[num]++;
         }
-        i=0;
-        for(auto sett: setting){
-            if(i+1 != sett){
-                ans.push_back(i+1);
-                break;
+        for(int i=1; i<=n; i++){
+            if(count[i] == 2){
+                dupli = i;
             }
-            i++;
+            else if(count[i] == 0){
+                missing = i;
+            }
         }
-        if(ans.size() != 2){
-            ans.push_back(nums.size());
-        }
-        return ans;
+        return {dupli, missing};
     }
 };
