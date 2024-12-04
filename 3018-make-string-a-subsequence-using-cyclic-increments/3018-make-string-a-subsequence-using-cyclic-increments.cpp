bb33f525176fc34f1d476e045eb2cf4042493a76
Priya Thakur <165200711+Priya-Thakur25@users.noreply.github.com>
2024-12-04 10:04:43 +0530

    Time: 7 ms (33.15%), Space: 17.2 MB (7.53%) - LeetHub

--- 3018-make-string-a-subsequence-using-cyclic-increments/3018-make-string-a-subsequence-using-cyclic-increments.cpp
+++ 3018-make-string-a-subsequence-using-cyclic-increments/3018-make-string-a-subsequence-using-cyclic-increments.cpp
@@ -6,8 +6,12 @@ public:
         int i=0,j=0;
         if(n2 > n1) return false;
         while(i != n1){
-            if(str1[i] == 'z' && str2[j] == 'a') j++;
-            else if(str1[i] == str2[j] ||  str1[i]+1 == str2[j]) j++;
+            if(str1[i] == 'z' && str2[j] == 'a'){
+                j++;
+            }
+            else if(str1[i] == str2[j] ||  str1[i]+1 == str2[j]){
+                j++;
+            }
             i++;
             if(i == n1 && j != n2) return false;
         }
