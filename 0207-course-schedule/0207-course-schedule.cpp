07903f8549e6880883b50735e2209d4761fc6bb1
Priya Thakur <165200711+Priya-Thakur25@users.noreply.github.com>
2024-12-11 14:30:20 +0530

    Time: 32 ms (6.48%), Space: 25.9 MB (5.06%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,59 @@
+class Graph{
+    public:
+    unordered_map<int,list<int>>adjList;
+    
+    void insert(int u, int v){
+        adjList[u].push_back(v);
+    }
+
+    void topoOrder(vector<int>&Order){
+        unordered_map<int,int>inDegree;
+        queue<int>q;
+
+        for(auto mp: adjList){
+            inDegree[mp.first] = 0;
+        }
+
+        for(auto i:adjList){
+            for(auto nbr: i.second){
+                inDegree[nbr]++;
+            }
+        }
+
+        for(auto i: adjList){
+            if(inDegree[i.first] == 0){
+                q.push(i.first);
+            }
+        }
+
+        while(!q.empty()){
+            int frontNode = q.front();
+            q.pop();
+            Order.push_back(frontNode);
+            for(auto nbr: adjList[frontNode]){
+                inDegree[nbr]--;
+                if(inDegree[nbr] == 0){
+                    q.push(nbr);
+                }
+            }
+        }
+
+    }
+};
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& p) {
+        Graph g;
+        if(p.size() == 0) return true;
+        for(auto i: p){
+            g.insert(i[1],i[0]);
+        }
+
+        for(int i=0; i<numCourses; i++){
+            g.adjList[i];
+        }
+        vector<int>Order;
+        g.topoOrder(Order);
+        return Order.size() == numCourses;
+    }
+};
