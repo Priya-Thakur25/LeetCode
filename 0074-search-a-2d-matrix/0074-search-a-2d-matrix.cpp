3154858cd3ab068ffc5802bf0e8e5968d27e6412
Priya Thakur <165200711+Priya-Thakur25@users.noreply.github.com>
2024-12-22 13:46:01 +0530

    Time: 0 ms (100%), Space: 13.3 MB (30.74%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& m, int target) {
+        int row = m.size();
+        int col = m[0].size();
+        int i=0,find = 0, finded = 0;
+        // if(row == 1 && col == 1) return 0;
+        for(i=0; i<row; i++){
+            if(m[i][0] > target){
+                find = i;
+                break;
+            }
+        }
+
+        for(int j=0; j<col; j++){
+            if(i-1 >=0 && m[i-1][j] == target){
+                finded = 1;
+            }
+        }
+        if(finded) return true;
+        return false;
+    }
+};
