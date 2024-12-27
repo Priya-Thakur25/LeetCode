8926279285b00561bf02e70889b24e07fded5867
Priya Thakur <165200711+Priya-Thakur25@users.noreply.github.com>
2024-12-27 18:02:52 +0530

    Time: 3 ms (91.65%), Space: 15 MB (54.39%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -17,21 +17,52 @@ public:
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
-        unordered_map<Node*,Node*>mp;
+
+        //FIRST APPROACH USING HASHMAP
+
+        // unordered_map<Node*,Node*>mp;
+        // Node* temp = head;
+        // while(temp != NULL){
+        //     Node* newNode = new Node(temp->val);
+        //     mp[temp] = newNode;
+        //     temp = temp->next;
+        // }
+        // temp = head;
+        // while(temp != NULL){
+        //     Node* node = mp[temp];
+        //     node->next = mp[temp->next];
+        //     node->random = mp[temp->random];
+        //     temp = temp->next;
+        // }
+
+        // return mp[head];
+
+        //SECOND INSERT-IN-BETWEEN APPROACH
         Node* temp = head;
         while(temp != NULL){
-            Node* newNode = new Node(temp->val);
-            mp[temp] = newNode;
-            temp = temp->next;
-        }
-        temp = head;
-        while(temp != NULL){
-            Node* node = mp[temp];
-            node->next = mp[temp->next];
-            node->random = mp[temp->random];
-            temp = temp->next;
+            Node* node = new Node(temp->val);
+            Node* nextNode = temp->next;
+            temp->next = node;
+            node->next = nextNode;
+            temp = temp->next->next;
         }
 
-        return mp[head];
+        temp = head;
+        while(temp != NULL){
+            Node* node = temp->next;
+            node->random = temp->random ? temp->random->next : NULL;
+            temp = temp->next->next;
+        }
+
+        temp = head;
+        Node* ans = NULL;
+        if(temp) ans = temp->next;
+        while(temp != NULL){
+            Node* node = temp->next;
+            temp->next = node->next;
+            if(temp->next) node->next = temp->next->next;
+            temp = temp->next;
+        }
+        return ans;
     }
 };
