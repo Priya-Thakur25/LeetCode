d0c6ffcf7346d1b5683294b16672f21c5fed6002
Priya Thakur <165200711+Priya-Thakur25@users.noreply.github.com>
2024-12-28 10:25:07 +0530

    Time: 1 ms (84.87%), Space: 14.7 MB (34.41%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,48 +1,20 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<pair<int,int>>v;
         int n = nums.size();
-        for(int i=0; i<n; i++){
-            v.push_back({nums[i],i});
+        vector<pair<int,int>>p;
+        for(int i=0; i<nums.size(); i++){
+            p.push_back({nums[i],i});
         }
 
-        sort(v.begin(), v.end());
-        int st=0, ed=n-1;
-        while(st < ed){
-            int sum = v[st].first + v[ed].first;
-            if(sum == target){
-                return {v[st].second, v[ed].second};
-            }
-            else if(sum < target) st++;
-            else ed--;
+        sort(p.begin(), p.end());
+        int s=0, e=n-1;
+        while(s < e){
+            int sum = p[s].first + p[e].first;
+            if(sum == target) return {p[s].second, p[e].second};
+            else if(sum > target) e--;
+            else s++;
         }
-        return {};
-        // int ans1, ans2, len = nums.size();
-        // vector<int>temp(nums.begin(), nums.end());
-        // sort(temp.begin(), temp.end());
-        // int s=0, e=len-1;
-        // vector<int>ans;
-        // while(s < e){
-        //     int add = temp[s]+temp[e];
-        //     if(add == target){
-        //        ans1 = temp[s];
-        //        ans2 = temp[e];
-        //        break;
-        //     }
-        //     else if(add > target){
-        //         e--;
-        //     }
-        //     else if(add < target){
-        //         s++;
-        //     }
-        // }
-
-        // for(int i=0; i<len; i++){
-        //     if(nums[i] == ans1 || nums[i] == ans2){
-        //         ans.push_back(i);
-        //     }
-        // }
-        // return ans;
+        return {-1,-1};
     }
 };
