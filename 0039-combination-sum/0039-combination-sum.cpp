b060e3c75f23216e64cdbf3a7dcf010adb09248c
Priya Thakur <165200711+Priya-Thakur25@users.noreply.github.com>
2024-12-30 14:34:58 +0530

    Time: 91 ms (8.51%), Space: 72.3 MB (5.49%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -9,13 +9,19 @@ void solve(vector<int>&can, vector<int>temp, vector<vector<int>>&ans, int target
         return;
     }
 
-    for(int i=index; i<can.size(); i++){
-        sum += can[i];
-        temp.push_back(can[i]);
-        solve(can, temp, ans, target, sum, i);
-        temp.pop_back();
-        sum -= can[i];
-    }
+    // for(int i=index; i<can.size(); i++){
+    //     sum += can[i];
+    //     temp.push_back(can[i]);
+    //     solve(can, temp, ans, target, sum, i);
+    //     temp.pop_back();
+    //     sum -= can[i];
+    // }
+
+    //using include exclude
+    temp.push_back(can[index]);
+    solve(can,temp,ans, target,sum+can[index],index);
+    temp.pop_back();
+    solve(can,temp,ans,target,sum,index+1);
 }
     vector<vector<int>> combinationSum(vector<int>& can, int target) {
         vector<int>temp;
