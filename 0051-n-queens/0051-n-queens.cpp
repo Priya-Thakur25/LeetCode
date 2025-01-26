6b31eda30fa26cc3ba046d92368c5f995085fcdf
Priya Thakur <165200711+Priya-Thakur25@users.noreply.github.com>
2025-01-26 13:50:45 +0530

    Time: 4 ms (38.87%), Space: 11.6 MB (27.97%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,58 +1,47 @@
 class Solution {
 public:
-unordered_map<int,bool>rowCheck;
-unordered_map<int,bool>LowDiag;
-unordered_map<int,bool>UpDiag;
-
-bool isSafe(vector<vector<char>>&board, int col, int row){
-    if(rowCheck[row] == true) return false;
-    if(LowDiag[row+col] == true) return false;
-    if(UpDiag[col-row] == true) return false;
-    return true;
-}
+unordered_map<int,bool>row;
+unordered_map<int,bool>uD;
+unordered_map<int,bool>lD;
 
 
-void store(vector<vector<string>>&ans, vector<vector<char>>&board){
-    vector<string>temp;
-    string s = "";
-    int n = board.size();
-    for(int i=0; i<n; i++){
-        s = "";
-        for(int j=0; j<n; j++){
-            s.push_back(board[i][j]);
+void store(vector<vector<char>>&board, vector<vector<string>>&final){
+    vector<string>tempans;
+    for(auto i: board){
+    string temp="";
+        for(auto j: i){
+            temp.push_back(j);
         }
-        temp.push_back(s);
+        tempans.push_back(temp);
     }
-    ans.push_back(temp);
+
+    final.push_back(tempans);
 }
-void solve(vector<vector<string>>&ans, vector<vector<char>>&board, int col, int n){
-    if(col >= n){
-        store(ans,board);
+void solve(int c, int n, vector<vector<char>>&board, vector<vector<string>>&final){
+    if(c == n){
+        store(board, final);
         return;
     }
 
-    for(int row=0; row<n; row++){
-        if(isSafe(board,col,row)){
-            board[row][col] = 'Q';
-            rowCheck[row] = true;
-            LowDiag[row+col] = true;
-            UpDiag[col-row] = true;
-
-            solve(ans, board, col+1, n);
-
-            board[row][col] = '.';
-            rowCheck[row] = false;
-            LowDiag[row+col] = false;
-            UpDiag[col-row] = false;
+    for(int i=0; i<n; i++){
+        if(row[i] == false && uD[c-i] == false && lD[i+c] == false){
+            row[i] = true;
+            uD[c-i] = true;
+            lD[i+c] = true;
+            board[i][c] = 'Q';
+            solve(c+1, n, board, final);
+            board[i][c] = '.';
+            row[i] = false;
+            uD[c-i] = false;
+            lD[i+c] = false;
         }
     }
-}
 
+}
     vector<vector<string>> solveNQueens(int n) {
-        vector<vector<string>>ans;
-        vector<vector<char>>board(n,vector<char>(n,'.'));
-        int col = 0;
-        solve(ans, board, col, n);
-        return ans;
+        vector<vector<char>>board(n, vector<char>(n, '.'));
+        vector<vector<string>>final;
+        solve(0, n, board, final);
+        return final;
     }
 };
