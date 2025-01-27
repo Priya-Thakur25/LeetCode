01971b1482761bb2559cef57f0ba8500e1fe73df
Priya Thakur <165200711+Priya-Thakur25@users.noreply.github.com>
2025-01-27 10:45:27 +0530

    Time: 0 ms (100%), Space: 8.3 MB (9.85%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -13,8 +13,25 @@ int solveUsingMem(int n, vector<int>&dp){
     dp[n] = solveUsingMem(n-1, dp) + solveUsingMem(n-2,dp);
     return dp[n];
 }
+
+//bottom-up approach
+int solveUsingTab(int n, vector<int>&dp){
+    if(n == 0) dp[0] = 0;
+    if(n >= 1){
+        dp[0] = 0;
+        dp[1] = 1;
+    }
+
+    for(int i=2; i<=n; i++){
+        dp[i] = dp[i-1] + dp[i-2];
+    }
+
+    return dp[n];
+}
     int fib(int n) {
         vector<int>dp(n+1,-1);
-        return solveUsingMem(n, dp);
+        // return solveUsingMem(n, dp);
+
+        return solveUsingTab(n,dp);
     }
 };
