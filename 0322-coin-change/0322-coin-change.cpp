f1efd765e53c9ee21a2b50c62e5fd5ce6f38057c
Priya Thakur <165200711+Priya-Thakur25@users.noreply.github.com>
2025-01-28 09:55:39 +0530

    Time: 36 ms (47.46%), Space: 19.1 MB (52.3%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,54 +1,45 @@
 class Solution {
 public:
-int coinRec(vector<int>& coins, int amount){
-    if(amount == 0) return 0;
+int solve(vector<int>&coins, int n){
+    if(n == 0) return 0;
 
     int mini = INT_MAX;
     for(int i=0; i<coins.size(); i++){
-        int recans = 0;
-        if((amount - coins[i]) >= 0){
-            recans = coinRec(coins, amount-coins[i]);
-            if(recans != INT_MAX){
-                int ans = 1 + recans;
-                 mini = min(mini,ans);
+        int temp = 0;
+        if(n - coins[i] >= 0){
+            temp = solve(coins, n-coins[i]);
+            if(temp != mini){
+            int ans = 1 + temp;
+            mini = min(mini, ans);
             }
         }
     }
     return mini;
 }
 
-int coinMemo(vector<int>& coins, int amount, vector<int>&dp){
-    if(amount == 0) return 0;
+int solveUsingMem(vector<int>&coins, int n, vector<int>&dp){
+    if(n == 0) return 0;
 
-    if(dp[amount] != -1) return dp[amount];
     int mini = INT_MAX;
+    if(dp[n] != -1) return dp[n];
     for(int i=0; i<coins.size(); i++){
-        int recans = 0;
-        if((amount - coins[i]) >= 0){
-            recans = coinMemo(coins, amount-coins[i], dp);
-            if(recans != INT_MAX){
-                int ans = 1 + recans;
-                 mini = min(mini,ans);
+        int temp = 0;
+        if(n - coins[i] >= 0){
+            temp = solveUsingMem(coins, n-coins[i], dp);
+            if(temp != INT_MAX){
+             int ans = 1 + temp;
+             mini = min(mini, ans);
             }
         }
     }
-    dp[amount] = mini;
-    return dp[amount];
+    dp[n] = mini;
+    return dp[n];
 }
     int coinChange(vector<int>& coins, int amount) {
-        // int ans = coinRec(coins, amount);
-        // if(ans != INT_MAX){
-        // return ans;
-        // }
-        // else return -1;
-
         int n = amount;
         vector<int>dp(n+1,-1);
-        int ans =  coinMemo(coins, amount, dp);
-        if(ans != INT_MAX){
+        int ans = solveUsingMem(coins, amount, dp);
+        if(ans == INT_MAX) return -1;
         return ans;
-        }
-        else return -1;
-
     }
 };
