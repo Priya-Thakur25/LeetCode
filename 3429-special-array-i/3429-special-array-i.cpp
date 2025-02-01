ff05079ecb37a36be3b133f33b0f8e05d2604de8
Priya Thakur <165200711+Priya-Thakur25@users.noreply.github.com>
2025-02-01 23:18:31 +0530

    Time: 0 ms (100%), Space: 27.7 MB (25%) - LeetHub

---
+++ 3429-special-array-i/3429-special-array-i.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool isArraySpecial(vector<int>& nums) {
+        int n = nums.size();
+        for(int i=0; i<n-1; ){
+            if((nums[i]%2 == 0 && nums[i+1]%2 == 1) || (nums[i]%2 == 1 && nums[i+1]%2 == 0)) i++;
+            else return false;
+        }
+        return true;
+    }
+};
