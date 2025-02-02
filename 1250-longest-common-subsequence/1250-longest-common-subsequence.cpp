063ae365d7f48e4f6bcfa4510ad64c539a79dc7e
Priya Thakur <165200711+Priya-Thakur25@users.noreply.github.com>
2025-02-02 09:12:13 +0530

    Time: 22 ms (80.7%), Space: 27.5 MB (50.72%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
@@ -36,12 +36,31 @@ public:
 
         return dp[0][0];
     }
+
+    int solveSO(string a, string b){
+        vector<int>curr(a.length()+1,0);
+        vector<int>next(a.length()+1,0);
+
+        for(int j=b.length()-1; j>=0; j--){
+            for(int i=a.length()-1; i>=0; i--){
+                int ans = 0;
+                if(a[i] == b[j]) ans = 1 + next[i+1];
+                else ans = 0 + max(curr[i+1], next[i]);
+                curr[i] = ans;
+            }
+            next = curr;
+        }
+
+        return next[0];
+    }
     int longestCommonSubsequence(string text1, string text2) {
         int i = 0;
         int j = 0;
         vector<vector<int>>dp(text1.length(), vector<int>(text2.length(),-1));
         // return solve(text1, text2, i, j);
         // return solveUsingMem(text1, text2, i, j, dp);
-        return solveUsingTab(text1, text2);
+        // return solveUsingTab(text1, text2);
+
+        return solveSO(text1, text2);
     }
 };
