f467c74ca4bd51ab69a9597a4710219fedb69ae8
Priya Thakur <165200711+Priya-Thakur25@users.noreply.github.com>
2025-02-02 17:22:51 +0530

    Time: 427 ms (15.55%), Space: 292.8 MB (8.51%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -0,0 +1,60 @@
+class Solution {
+public: 
+    int len = 0;
+    int index = 0;
+    bool isPalindrome(string &s, int st, int en){
+        int i = st;
+        int j = en;
+        while(st < en){
+            if(s[st] != s[en]) return false;
+            st++;
+            en--;
+        }
+        return true;
+    }
+
+    int solve(string &s, int st, int en){
+        if(st > en) return 0;
+
+        if(isPalindrome(s,st,en)){
+            if(len < en-st+1){
+                index = st;
+                len = en-st+1;
+            }
+            return en-st+1;
+        }
+        int opt1 = solve(s, st+1, en);
+        int opt2 = solve(s, st, en-1);
+        return max(opt1,opt2);
+    }
+
+    int solveUsingMem(string& s, int i, int j, vector<vector<int>>&dp){
+        if(i > j) return 0;
+
+        if(dp[i][j] != -1) return dp[i][j];
+
+        if(isPalindrome(s,i,j)){
+            if(len < j-i+1){
+                index = i;
+                len = j-i+1;
+            }
+            return j-i+1;
+        }
+        int opt1 = solveUsingMem(s, i+1, j,dp);
+        int opt2 = solveUsingMem(s, i, j-1,dp);
+        dp[i][j] = max(opt1,opt2);
+        return dp[i][j];
+    }
+
+
+    string longestPalindrome(string s) {
+        int n = s.length();
+        int st = 0;
+        int en = n-1;
+        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
+        // solve(s, st, en);
+        solveUsingMem(s,st,en,dp);
+        string ans = s.substr(index, len);
+        return ans;
+    }
+};
