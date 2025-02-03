38fa69e5bde160f7c6aea37147330c1817f370f3
Priya Thakur <165200711+Priya-Thakur25@users.noreply.github.com>
2025-02-03 15:13:30 +0530

    Time: 3 ms (76.02%), Space: 9.9 MB (99.03%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -1,38 +1,44 @@
 class Solution {
 public:
     int romanToInt(string s) {
-        int n = s.length();
         int i=0;
-        int sum = 0;
+        int n = s.length();
+        int ans = 0;
         while(i < n){
             char ch = s[i];
-            if(i+1 != n && ch == 'I' && s[i+1] == 'V'){
-                sum += 4; i+=2; continue;
+            if(i+1 <n && ch == 'I' && s[i+1] == 'V'){
+                ans = ans + 4;
+                i++;
             }
-            else if(i+1 != n && ch == 'I' && s[i+1] == 'X'){
-                sum += 9; i+=2; continue;
+            else if(i+1 <n && ch == 'I' && s[i+1] == 'X'){
+                ans = ans + 9;
+                i++;
             }
-            else if(i+1 != n && ch == 'X' && s[i+1] == 'L'){
-                sum += 40; i+=2; continue;
+            else if(i+1 <n && ch == 'X' && s[i+1] == 'L'){
+                ans = ans + 40;
+                i++;
             }
-            else if(i+1 != n && ch == 'X' && s[i+1] == 'C'){
-                sum += 90; i+=2; continue;
+            else if(i+1 <n && ch == 'X' && s[i+1] == 'C'){
+                ans = ans + 90;
+                i++;
             }
-            else if(i+1 != n && ch == 'C' && s[i+1] == 'D'){
-                sum += 400; i+=2; continue;
+            else if(i+1 <n && ch == 'C' && s[i+1] == 'D'){
+                ans = ans + 400;
+                i++;
             }
-            else if(i+1 != n && ch == 'C' && s[i+1] == 'M'){
-                sum += 900; i+=2; continue;
+            else if(i+1 <n && ch == 'C' && s[i+1] == 'M'){
+                ans = ans + 900;
+                i++;
             }
-            else if(ch == 'I')sum += 1;
-            else if(ch == 'V') sum += 5;
-            else if(ch == 'X') sum += 10;
-            else if(ch == 'L') sum += 50;
-            else if(ch == 'C') sum += 100;
-            else if(ch == 'D') sum += 500;
-            else if(ch == 'M') sum += 1000;
+            else if(ch == 'I') ans = ans + 1;
+            else if(ch == 'V') ans = ans + 5;
+            else if(ch == 'X') ans = ans + 10;
+            else if(ch == 'L') ans = ans + 50;
+            else if(ch == 'C') ans = ans + 100;
+            else if(ch == 'D') ans = ans + 500;
+            else if(ch == 'M') ans = ans + 1000;
             i++;
         }
-        return sum;
+        return ans;
     }
 };
