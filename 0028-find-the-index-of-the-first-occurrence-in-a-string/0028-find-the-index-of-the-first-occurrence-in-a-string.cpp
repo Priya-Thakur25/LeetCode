bb538c82b1e2f0b2d35a74b0b7a5ec13179f3915
Priya Thakur <165200711+Priya-Thakur25@users.noreply.github.com>
2025-02-03 17:18:57 +0530

    Time: 0 ms (100%), Space: 8.5 MB (95.95%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,21 +1,7 @@
 class Solution {
 public:
-    int strStr(string s1, string s2) {
-        int i=0,n1 = s1.length();
-        int j=0,n2 = s2.length();
-        int flag = 0;
-        for(i=0; i<n1; i++){
-            flag = 0;
-            for(j=0; j<n2; j++){
-                if(s1[i+j] != s2[j]){
-                    flag = 1;
-                    break;
-                }
-            }
-            if(flag == 0){
-                return i;
-            }
-        }
-        return -1;
+    int strStr(string h, string n) {
+        if(h.find(n) == string::npos) return -1;
+        return h.find(n);
     }
 };
