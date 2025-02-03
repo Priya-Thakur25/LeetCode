21fd2cc0fe859159aa0ef0f6bce7ba45cce9b97e
Priya Thakur <165200711+Priya-Thakur25@users.noreply.github.com>
2025-02-03 16:42:28 +0530

    Time: 0 ms (100%), Space: 12.1 MB (22.64%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,64 +1,17 @@
-class Trie{
-    public:
-    char val;
-    Trie* children[26];
-    bool isTerminal;
-    
-    Trie(char val){
-        this->val = val;
-        for(int i=0; i<26; i++){
-            this->children[i] = NULL;
-        }
-        this->isTerminal = false;
-    }
-};
-
-//insertion
-void insertion(Trie* root, string word){
-    if(word.length() == 0){
-        root->isTerminal = true;
-        return;
-    }
-    
-    char ch = word[0];
-    int index = ch - 'a';
-    Trie* child;
-    if(root->children[index] != NULL){
-        //already present a character
-        child = root->children[index];
-    }
-    else{
-        //bana li h bs fill krdo
-        child = new Trie(ch);
-        root->children[index] = child;
-    }
-    //recursive call 
-    insertion(child, word.substr(1));
-}
-
-void helper(Trie* root, int count, string &ans){
-    if(root == NULL || root->isTerminal) return;
-    int index = 0;
-    for(int i=0 ; i<26; i++){
-        if(root->children[i] != NULL){
-            index = i;
-            count++;
-        }
-    }
-    if(count > 1) return;
-    if(count == 1) ans = ans+root->children[index]->val;
-    helper(root->children[index], 0, ans);
-}
 class Solution {
 public:
-    string longestCommonPrefix(vector<string>& strs) {
-        Trie* temp = new Trie('-');
-        for(int i=0 ;i<strs.size(); i++){
-            insertion(temp, strs[i]);
+    string longestCommonPrefix(vector<string>& s) {
+        int n = s.size();
+        sort(s.begin(), s.end());
+        int i=0;
+        string ans = "";
+        while(i < s[0].length() && i < s[n-1].length()){
+            string first = s[0];
+            string sec = s[n-1];
+            if(first[i] != sec[i]) break;
+            else ans += first[i];
+            i++;
         }
-        string tempstr = "";
-        int count = 0;
-        helper(temp, count, tempstr);
-        return tempstr;
+        return ans;
     }
 };
