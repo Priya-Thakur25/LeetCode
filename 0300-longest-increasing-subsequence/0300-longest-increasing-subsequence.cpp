342affb9def21852b61e281e7799640320355e27
Priya Thakur <165200711+Priya-Thakur25@users.noreply.github.com>
2025-02-05 08:10:33 +0530

    Time: 575 ms (11.49%), Space: 296.9 MB (6.63%) - LeetHub

---
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int solve(vector<int>&nums, int prev, int curr){
+        if(curr >= nums.size()) return 0;
+
+        int op1 = 0;
+        if(prev == -1 || nums[prev] < nums[curr]) op1 = 1 + solve(nums, curr, curr+1);
+        int op2 = 0 + solve(nums,prev,curr+1);
+        return max(op1,op2);
+    }
+
+    int solveUsingMemo(vector<int>&nums, int prev, int curr, vector<vector<int>>&dp){
+        if(curr >= nums.size()) return 0;
+
+        if(dp[prev+1][curr] != -1) return dp[prev+1][curr];
+        int op1 = 0;
+        if(prev == -1 || nums[prev] < nums[curr]) op1 = 1 + solveUsingMemo(nums, curr, curr+1,dp);
+        int op2 = 0 + solveUsingMemo(nums,prev,curr+1,dp);
+        dp[prev+1][curr] = max(op1,op2);
+        return dp[prev+1][curr];
+    }
+
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        // return solve(nums,-1,0);
+        vector<vector<int>>dp(n+1, vector<int>(n+1,-1));
+        return solveUsingMemo(nums,-1,0,dp);
+    }
+};
