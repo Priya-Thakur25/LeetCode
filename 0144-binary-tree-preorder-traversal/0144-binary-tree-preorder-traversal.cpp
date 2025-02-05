5ed560be89a1efb7048b3c878a2495f80e8196f6
Priya Thakur <165200711+Priya-Thakur25@users.noreply.github.com>
2025-02-05 15:39:02 +0530

    Time: 0 ms (100%), Space: 10.9 MB (72.5%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
@@ -11,16 +11,41 @@
  */
 class Solution {
 public:
-    void preOrder(TreeNode* root, vector<int>&ans){
-        if(root == NULL) return;
+    // void preOrder(TreeNode* root, vector<int>&ans){
+    //     if(root == NULL) return;
 
-        ans.push_back(root->val);
-        preOrder(root->left, ans);
-        preOrder(root->right, ans);
-    }
+    //     ans.push_back(root->val);
+    //     preOrder(root->left, ans);
+    //     preOrder(root->right, ans);
+    // }
     vector<int> preorderTraversal(TreeNode* root) {
         vector<int>ans;
-        preOrder(root,ans);
+        // preOrder(root,ans);
+        // return ans;
+
+        //MORRIS TRSVERSAL
+        TreeNode* curr = root;
+        while(curr != NULL){
+            if(curr->left == NULL){
+                ans.push_back(curr->val);
+                curr = curr->right;
+            }
+            else{
+                 TreeNode* prev = curr->left;
+                 while(prev->right && prev->right != curr){
+                    prev = prev->right;
+                 }
+                 if(prev->right == NULL){
+                    ans.push_back(curr->val);
+                    prev->right = curr;
+                    curr = curr->left;
+                 }
+                 else{
+                    prev->right = NULL;
+                    curr = curr->right;
+                 }
+            }
+        }
         return ans;
     }
 };
