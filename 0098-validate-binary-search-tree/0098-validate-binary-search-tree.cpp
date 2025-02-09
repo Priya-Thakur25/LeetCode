ea2081f3b8922015ba355a6c6aafada4acb3fd8b
Priya Thakur <165200711+Priya-Thakur25@users.noreply.github.com>
2025-02-09 16:41:51 +0530

    Time: 0 ms (100%), Space: 22.1 MB (19.27%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -11,19 +11,16 @@
  */
 class Solution {
 public:
-bool checkBST(TreeNode* root, long long left, long long right){
-    if(root == NULL){
-        return 1;
+    bool solve(TreeNode* root, long long  mini, long long maxi){
+        if(root == NULL) return 1;
+        bool leftkaans = 0, rightkaans = 0, ans = 0;
+        if(root->val < maxi && root->val > mini) ans = 1;
+        leftkaans = solve(root->left, mini, root->val);
+        rightkaans = solve(root->right, root->val, maxi);
+        return ans && leftkaans && rightkaans;
     }
-    bool ans = 0;
-    if(root->val > left && root->val < right){
-         ans = checkBST(root->left, left, root->val) && checkBST(root->right, root->val, right);
-    }
-    return ans;
-}
     bool isValidBST(TreeNode* root) {
-        long long left = -2147483657;
-        long long  right = 2147483657;
-        return checkBST(root, left, right);
+    long long mini = -2147483657, maxi = 2147483657;
+    return solve(root,mini,maxi);
     }
 };
