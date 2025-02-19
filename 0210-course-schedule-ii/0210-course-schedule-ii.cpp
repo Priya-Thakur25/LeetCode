2805301d0f824f482163678522616a32936260b5
Priya Thakur <165200711+Priya-Thakur25@users.noreply.github.com>
2025-02-19 15:53:23 +0530

    Time: 24 ms (5.18%), Space: 20.4 MB (6.07%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,59 +1,48 @@
-class Graph{
-    public:
-    unordered_map<int,list<int>>adjList;
-    
-    void insert(int u, int v){
-        adjList[u].push_back(v);
-    }
+class Graph {
+public:
+    unordered_map<int, vector<int>> adjList;
 
-    void topoOrder(vector<int>&Order){
-        unordered_map<int,int>inDegree;
-        queue<int>q;
-
-        for(auto mp: adjList){
-            inDegree[mp.first] = 0;
-        }
-
-        for(auto i:adjList){
-            for(auto nbr: i.second){
-                inDegree[nbr]++;
-            }
-        }
-
-        for(auto i: adjList){
-            if(inDegree[i.first] == 0){
-                q.push(i.first);
-            }
-        }
-
-        while(!q.empty()){
-            int frontNode = q.front();
-            q.pop();
-            Order.push_back(frontNode);
-            for(auto nbr: adjList[frontNode]){
-                inDegree[nbr]--;
-                if(inDegree[nbr] == 0){
-                    q.push(nbr);
-                }
-            }
-        }
-
-    }
+    void insertEdge(int u, int v) { adjList[u].push_back(v); }
 };
+
+void topo(int src, vector<int>& ans, unordered_map<int, vector<int>>& adjList) {
+    unordered_map<int, int> indegree;
+    for (auto i : adjList)
+        indegree[i.first] = 0;
+    for (auto i : adjList) {
+        for (auto j : i.second) {
+            indegree[j]++;
+        }
+    }
+
+    queue<int> q;
+    for (auto i : indegree) {
+        if (indegree[i.first] == 0)
+            q.push(i.first);
+    }
+
+    while (!q.empty()) {
+        int front = q.front();
+        q.pop();
+        ans.push_back(front);
+        for (auto nbr : adjList[front]) {
+            indegree[nbr]--;
+            if (indegree[nbr] == 0)
+                q.push(nbr);
+        }
+    }
+}
 class Solution {
 public:
     vector<int> findOrder(int numCourses, vector<vector<int>>& p) {
         Graph g;
-        for(auto i: p){
-            g.insert(i[1],i[0]);
+        for (auto i : p) {
+            g.insertEdge(i[1], i[0]);
         }
-
-        for(int i=0; i<numCourses; i++){
-            g.adjList[i];
-        }
-        
-        vector<int>Order;
-        g.topoOrder(Order);
-        return Order.size() == numCourses ? Order : vector<int>{};
+        for(int i=0; i<numCourses; i++) g.adjList[i];
+        vector<int> ans;
+        topo(0, ans, g.adjList);
+        if(ans.size() != numCourses) return {};
+        return ans;
     }
 };
