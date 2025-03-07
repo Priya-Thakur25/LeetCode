d7584890a8d2d2158a63e376f1242cc65250161d
Priya Thakur <165200711+Priya-Thakur25@users.noreply.github.com>
2025-03-07 14:25:07 +0530

    Time: 0 ms (100%), Space: 8.7 MB (17.12%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -7,6 +7,12 @@ public:
     }
     int climbStairs(int n) {
         vector<int>dp(n+1,-1);
-        return solveUsingMem(dp,n);
+        // return solveUsingMem(dp,n);
+        dp[1] = 1;
+        if(n >= 2) dp[2] = 2;
+        for(int i=3; i<=n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
     }
 };
