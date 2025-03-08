97dc99395cec87c24a2ad77537522af56806092d
Priya Thakur <165200711+Priya-Thakur25@users.noreply.github.com>
2025-03-08 14:01:51 +0530

    Time: 0 ms (100%), Space: 10.6 MB (62.1%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,42 +1,23 @@
 class Solution {
 public:
-// int solve(int n, vector<int>&nums, vector<int>&dp){
-//     if(n >= nums.size()) return 0;
-
-//     if(dp[n] != -1) return dp[n];
-//     int inc = nums[n] + solve(n+2, nums, dp);
-//     int exc = 0 + solve(n+1, nums, dp);
-//     dp[n] = max(inc, exc);
-//     return dp[n];
-// }
+    int solve(int i, vector<int>&nums){
+        if(i >= nums.size()) return 0;
+        int include = nums[i] + solve(i+2,nums);
+        int exclude = 0 + solve(i+1, nums);
+        return max(include,exclude);
+    }
+    int solveUsingMem(int i, vector<int>&nums, vector<int>&dp){
+        if(i >= nums.size()) return 0;
+        if(dp[i] != -1) return dp[i];
+        int include = nums[i] + solveUsingMem(i+2,nums,dp);
+        int exclude = 0 + solveUsingMem(i+1,nums,dp);
+        return dp[i] = max(include,exclude);
+    }
     int rob(vector<int>& nums) {
         int n = nums.size();
-        // vector<int>dp(n, -1);
-        // // return solve(0, nums, dp);
-
-        // dp[n-1] = nums[n-1];
-        // for(int i=n-2; i>=0; i--){
-        //     int temp = 0;
-        //     if(i+2 < n) temp = dp[i+2];
-        //     int inc = nums[i] + temp;
-        //     int exc = 0 + dp[i+1];
-        //     dp[i] = max(inc, exc); 
-        // }
-        // return dp[0];
-
-        //SPACE OPTIMIZATION -- curr -> prev -> next
-        int prev = nums[n-1];
-        int next = 0;
-        int curr = 0;
-        for(int i=n-2; i>=0; i--){
-            int temp = 0;
-            if(i+2 < n) temp = next;
-            int inc = nums[i] + temp;
-            int exc = 0 + prev;
-            curr = max(inc, exc);
-            next = prev;
-            prev = curr;
-        }
-        return prev;
+        int index = 0;
+        // return solve(index,nums);
+        vector<int>dp(n+1,-1);
+        return solveUsingMem(index,nums,dp);
     }
 };
