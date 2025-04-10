3f5ba0e823f145bb1334596cbfecc74e579ed6bb
Priya Thakur <165200711+Priya-Thakur25@users.noreply.github.com>
2025-04-10 07:52:59 +0530

    Time: 0 ms (100%), Space: 16.2 MB (35.11%) - LeetHub

---
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int solve(int i, int buy, vector<int>&prices){
+        int n = prices.size();
+        if(i >= n) return 0;
+
+        int profit  = 0;
+        if(buy){
+            profit = max(-prices[i]+solve(i+1,0,prices), solve(i+1,1,prices));
+        }
+        else profit = max(prices[i]+solve(i+2,1,prices), solve(i+1,0,prices));
+        return profit;
+    }
+    int solveUsingMem(int i, int buy, vector<int>&prices, vector<vector<int>>&dp){
+        int n = prices.size();
+        if(i >= n) return 0;
+        if(dp[i][buy] != -1) return dp[i][buy];
+        int profit  = 0;
+        if(buy){
+            profit = max(-prices[i]+solveUsingMem(i+1,0,prices,dp), solveUsingMem(i+1,1,prices,dp));
+        }
+        else profit = max(prices[i]+solveUsingMem(i+2,1,prices,dp), solveUsingMem(i+1,0,prices,dp));
+        return dp[i][buy] = profit;
+    }
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        vector<vector<int>>dp(n,vector<int>(2,-1));
+        return solveUsingMem(0,1,prices,dp);
+    }
+};
